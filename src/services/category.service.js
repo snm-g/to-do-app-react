67311ba133eda04a9ef9edc6711e8475d0647b5c
Sergio_NM <180465994+snm-g@users.noreply.github.com>
2026-02-25 12:42:05 -0400

    Update with corrections category.service.js

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -6,30 +6,13 @@ export const getAll = async () => {
 };
 
 export const create = async (nuevaCategoria) => {
-  const respuesta = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(nuevaCategoria),
-  });
-  return await respuesta.json();
+
 };
 
 export const update = async (id, categoriaActualizada) => {
-  const respuesta = await fetch(`${API_URL}/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(categoriaActualizada),
-  });
-  return await respuesta.json();
+
 };
 
 export const remove = async (id) => {
-  const respuesta = await fetch(`${API_URL}/${id}`, {
-    method: "DELETE",
-  });
-  return respuesta.ok;
+
 };
