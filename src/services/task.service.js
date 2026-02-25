656bc4e0d36ee2853dc78afc9b922c6d9c75a792
Sergio_NM <180465994+snm-g@users.noreply.github.com>
2026-02-25 12:22:04 -0400

    Update with corrections task.service.js

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,32 +1,16 @@
 const API_URL = 'http://127.0.0.1:8000/api/tasks';
 
 export const create = async (nuevaTarea) => {
-  const respuesta = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(nuevaTarea)
-  });
-  return await respuesta.json();
+
 };
 
 export const update = async (id, tareaActualizada) => {
-  const respuesta = await fetch(`${API_URL}/${id}`, {
-    method: 'PUT', 
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(tareaActualizada)
-  });
-  return await respuesta.json();
+
 };
 
 export const remove = async (id) => {
-  const respuesta = await fetch(`${API_URL}/${id}`, {
-    method: 'DELETE'
-  });
-  return respuesta.ok; 
+
 };
+
 
 
